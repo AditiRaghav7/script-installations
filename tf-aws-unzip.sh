204590cc3f0d732cbdc2cdd01c118cf8a5e307a3
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-03-04 15:12:07 +0530

    Update tf-aws-unzip.sh

--- tf-aws-unzip.sh
+++ tf-aws-unzip.sh
@@ -13,7 +13,6 @@ echo "Installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-rm -rf awscliv2.zip aws/
 
 # Install Terraform
 echo "Installing Terraform..."
@@ -22,7 +21,6 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/trusted.gpg.
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt update && sudo apt install -y terraform
-
 
 
 # Verify Installations
